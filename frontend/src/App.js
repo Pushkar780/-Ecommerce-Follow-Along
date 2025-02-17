140ba5daf3db7b4f6cb3f42dca7988baa118b64b
Pushkar <git config --global user.email>
2025-02-17 14:36:23 +0530

    milestone-7

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { LoginPage, SignupPage, ActivationPage } from "./Routess.js"; // Import fixed
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/sign-up" element={<SignupPage />} />
+        <Route
+          path="/activation/:activation_token"
+          element={<ActivationPage />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ frontend/src/Routess.js
@@ -0,0 +1,4 @@
+import LoginPage from "../src/pages/login.jsx";
+import SignupPage from "../src/pages/SignupPage.jsx";
+import ActivationPage from "../src/pages/ActivationPage.jsx";
+export { LoginPage, SignupPage, ActivationPage };
