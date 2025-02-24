bd9a3196aa2a85323593a12d502fb43d528aca15
Pushkar <git config --global user.email>
2025-02-24 12:57:46 +0530

    milestone-13

--- frontend/src/Routess.js
+++ frontend/src/Routess.js
@@ -1,9 +1,14 @@
-import LoginPage from "../src/pages/login.jsx"
-
-
-export
-{
-    LoginPage,
-   
-}
-
+import LoginPage from "../src/pages/login.jsx";
+import SignupPage from "../src/pages/SignupPage.jsx";
+import ActivationPage from "../src/pages/ActivationPage.jsx";
+import Home from "../src/pages/Home.jsx";
+import CreateProduct from "../src/pages/createProduct.jsx";
+import MyProducts from "../src/pages/myProducts.jsx";
+export {
+  LoginPage,
+  Home,
+  SignupPage,
+  ActivationPage,
+  CreateProduct,
+  MyProducts,
+};
